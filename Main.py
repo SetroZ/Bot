4e2ce11d8e2d5e84918e9be29f8669f9014b6904
SetroZ <yousfsbry66@gmail.com>
2021-03-19 15:02:48 +0300

    added error handling

--- Main.py
+++ Main.py
@@ -1,21 +1,20 @@
-from Variables import *
+from Rules import *
+
 from discord.ext import commands
 import discord
 import requests
 import json
-import time
 
-client=commands.Bot(command_prefix=".")
+
+client = commands.Bot(command_prefix=".")
+
 
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
-    json_data =json.loads(response.text)
-    quote=json_data[0]['q']+" -"+json_data[0]['a']
-    return(quote)
-
-
-
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return (quote)
 
 
 @client.event
@@ -23,9 +22,15 @@ async def on_ready():
     print("Ready")
 
 
+@client.event
+async def on_messsage(message):
+    if message.author.id == 817426689847590942:
+        await message.channel.purge(limit=1)
+
+
 @client.command()
 async def inspire(ctx):
-    quote=get_quote()
+    quote = get_quote()
     await ctx.send(quote)
 
 
@@ -33,41 +38,46 @@ async def inspire(ctx):
 async def Hello(ctx):
     await ctx.send("Hi")
 
+
 @client.command()
 async def rules(ctx):
     await ctx.send(Rules)
 
-@client.command()
-async def send(ctx):
-    await ctx.send(".rule")
 
 @client.command()
-async def rule(ctx,*,number):
-    await ctx.send(R[int(number)-1])
+async def send(ctx):
+    msg = await ctx.send(".rule")
+    await msg.delete()
+
+
+@client.command()
+async def rule(ctx, *, number):
+    await ctx.send(R[int(number) - 1])
+
 
 @client.command()
 @commands.has_permissions(manage_messages=True)
-async def clear(ctx,amount=2):
+async def clear(ctx, amount=2):
     await ctx.channel.purge(limit=amount)
     await ctx.send(f"Deleted {amount} messages")
 
 
-
-
 @client.command()
 @commands.has_permissions(manage_messages=True)
-async def clear_all (ctx,amount=99999999):
+async def clear_all(ctx, amount=99999999):
     await ctx.channel.purge(limit=amount)
+
 
 @client.command()
 @commands.has_permissions(kick_members=True)
-async def kick (ctx,member : discord.Member,*,reason= "No reason provided"):
+async def kick(ctx, member: discord.Member, *, reason="No reason provided"):
     await member.send(f"You have been Kicked , Because {reason}")
     await member.kick(reason=reason)
 
+
 @client.command()
 @commands.has_permissions(ban_members=True)
-async def ban (ctx,member : discord.Member,*,reason= "No reason provided"):
+async def ban(ctx, member: discord.Member, *, reason="No reason provided"):
     try:
         await member.send(f"You have been banned, Because {reason}\n {b}")
     except:
@@ -76,41 +86,46 @@ async def ban (ctx,member : discord.Member,*,reason= "No reason provided"):
         await ctx.send(f"{member} has been banned \n {b}")
         await member.ban(reason=reason)
 
+
 @client.command()
 @commands.has_permissions(ban_members=True)
-async def unban(ctx,*,member):
-    banned_users=await ctx.guild.bans()
-    member_name,member_disc=member.split('#')
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_disc = member.split('#')
     for banned_entry in banned_users:
-        user=banned_entry.user
-        if(user.name,user.discriminator)==(member_name,member_disc):
-
+        user = banned_entry.user
+        if (user.name, user.discriminator) == (member_name, member_disc):
             await ctx.guild.unban(user)
-            await ctx.send(member_name +" has been unbanned!")
+            await ctx.send(member_name + " has been unbanned!")
             return
-    await ctx.send(member+ " was not found")
+    await ctx.send(member + " was not found")
+
+mute_role=False
 @client.command()
 @commands.has_permissions(kick_members=True)
-async def mute (ctx,member:discord.Member):
-    muted=ctx.guild.get_role(821972019176144977)
+async def mute(ctx, member: discord.Member):
+    muted = ctx.guild.get_role(821972019176144977)
     await member.add_roles(muted)
-    await ctx.send(member.mention +" Has Been Muted")
-    await ctx.channel.purge(limit=1)
+    global mute_role
+
+    if mute_role == True:
+        await ctx.send(member.mention + "Is already muted")
+    else:
+        mute_role=True
+        await ctx.send(member.mention + "Has Been Muted")
+
+
 @client.command()
 @commands.has_permissions(kick_members=True)
-async def unmute(ctx, member:discord.Member):
+async def unmute(ctx, member: discord.Member):
     muted = ctx.guild.get_role(821972019176144977)
     await member.remove_roles(muted)
-    await ctx.send(member.mention +"Has Been Unmuted")
-
-
-
-
-
-
-
-
-
+    global mute_role
+    if mute_role == False:
+        await ctx.send(member.mention + " Is already unmuted")
+    else:
+        mute_role=False
+        await ctx.send(member.mention + "Has been unmuted ")
 
 
 
